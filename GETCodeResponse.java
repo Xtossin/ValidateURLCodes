37e578ff69474bf7b3ed7f4f5089cb017381f8ea
xtossin <christospavlatos@gmail.com>
2018-04-11 00:23:36 -0700

    Refactored GetCodeResponse.java

--- GETCodeResponse.java
+++ GETCodeResponse.java
@@ -2,43 +2,51 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.*;
 import java.util.Objects;
-import java.util.logging.Logger;
-
-import org.junit.Before;
-import org.junit.Test;
 
 public class GETCodeResponse {
 
 	public static void main(String[] args) throws Exception {
 
-		String fileContainingUrl = "sites.txt";
-		String siteLine = "";
+		String fileContainingUrl = "associations.csv";
+
+		String siteLine = null;
+		String domain = "https://pcth6fm1ud.execute-api.us-west-1.amazonaws.com/dev/";
 
 		FileReader urlToTest = new FileReader(fileContainingUrl);
 		BufferedReader bufferedReader = new BufferedReader(urlToTest);
 
-		while ((siteLine = bufferedReader.readLine()) != null) {
-			URL uri = new URL(siteLine);
-			HttpURLConnection urlConnection = (HttpURLConnection) uri.openConnection();
-			urlConnection.setRequestMethod("GET");
-			String newUrl = urlConnection.getHeaderField("Location");
-			
+		while (bufferedReader.readLine() != null) {
 
-			String httpsAdd = "https://";
-			String sampleString = siteLine;
-			String[] items = sampleString.split("(?<=http://)");
-			String itemLista = httpsAdd + items[1];
+			String splitBy = ",";
+			siteLine = domain + bufferedReader.readLine();
+			String[] b = siteLine.split(splitBy);
+			String expectedUrl = b[1];
+			System.out.println(b[0]);
+			URL uri = new URL(b[0]);
+			HttpURLConnection urlConnection = (HttpURLConnection) uri.openConnection();
+
+			urlConnection.setRequestMethod("GET");
+			urlConnection.setInstanceFollowRedirects(false);
+			HttpURLConnection.setFollowRedirects(false);
+
+			String uriInString = uri.toString();
+
+			String newUrl = urlConnection.getHeaderField("Location");
+			System.out.println("Location : " + newUrl);
 
 			int code = urlConnection.getResponseCode();
 
-			PassedORfailed.passOrFail(itemLista, itemLista, code);
-				System.out.println(PassedORfailed.passOrFail(newUrl, itemLista, code));
-				System.out.println("URL Tested      : " + siteLine);
-				System.out.println("Location of URL : " + newUrl);
-				System.out.println("Response Code   : " + code + "\n");
-				
-			urlConnection.connect();
+			System.out.println("Response Code   : " + code);
+
+			boolean testResult = passOrFail(newUrl, expectedUrl, code);
+			System.out.println(testResult + "\n");
+			urlConnection.disconnect();
+
 		}
 		bufferedReader.close();
+	}
+
+	public static boolean passOrFail(String newUrl, String expectedUrl, int code) {
+		return (Objects.equals(newUrl, expectedUrl) && code == 301);
 	}
 }
